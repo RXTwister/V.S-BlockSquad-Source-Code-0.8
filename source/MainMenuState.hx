86d44990c8ecd14ef2d0d7e5397ba45d2ad903b5
RXTwister <94095972+RXTwister@users.noreply.github.com>
2022-01-16 21:10:32 +0800

    1.2.5

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -24,7 +24,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var vsBlocksquad:String = '1.0';
+	public static var vsBlocksquad:String = '1.2.5';
 	public static var psychEngineVersion:String = '0.4.2'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
